0920a3ffeee6f699bac37b14d48983d829d828df
canmizrakli <105656251+canmizrakli@users.noreply.github.com>
2022-08-23 02:28:31 +0300

    Update main.java

--- main.java
+++ main.java
@@ -47,4 +47,99 @@ public class main {
         System.out.println("Your Average Is: " + result);
     }
 
+    public static void byLetter() {
+         Scanner scan = new Scanner(System.in);
+         
+         
+         System.out.println("Would you like to calculate your grades by AA, BA, BB grading system or A, A-, B+ grading system? Type double for the first one and single for the second one");
+         String choice = scan.next();
+         if (choice.equalsIgnoreCase("double")){
+          startDouble();                                                      
+         }
+         else if (choice.equalsIgnoreCase("single")) {
+          startSingle();
+         }
+         else {
+            System.out.println("WRONG INPUT! TRY AGAIN!!!!");
+            startMenu();
+        }
+    }
+
+    private static void startSingle() {
+        
+        String gradeStr = " ";
+        double gradeVal = 0;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of classes you have");
+        int classNo = scan.nextInt();
+        System.out.println("Please enter the credits for classes in an order you have chosen");
+        double allCred[] = new double [classNo];
+        for (int i = 0; i<classNo; i++){
+            allCred[i] = scan.nextInt();
+        }
+        System.out.println("Please enter your grades in the same order as the credits you entered beforehand");
+        String singleGrades[] = new String [classNo];
+        for (int i = 0; i<classNo; i++){
+            singleGrades[i] = scan.next();
+        }
+
+        switch (gradeStr) {
+            case "A": 
+                gradeVal = 4.0;
+                break;
+            case "A-": 
+                gradeVal = 3.7;
+                break;
+            case "B+": 
+                gradeVal = 3.3;
+                break;    
+            case "B": 
+                gradeVal = 3.0;
+                break;
+            case "B-": 
+                gradeVal = 2.7;
+                break;    
+            case "C+": 
+                gradeVal = 2.3;
+                break;    
+            case "C": 
+                gradeVal = 2.0;
+                break;
+            case "C-": 
+                gradeVal = 1.7;
+                break;    
+            case "D+":
+                gradeVal = 1.3;
+                break;
+            case "D":
+                gradeVal = 1.0;
+                break;    
+            case "F":
+                gradeVal = 0;
+                break;    
+            case "FX":
+                gradeVal = 0;
+                break;      
+            case "FZ":
+                gradeVal = 0;
+                break;      
+                
+        }
+
+
+        for (int i = 0; i<classNo; i++) {
+            
+        }
+
+        for (int i=0; i<classNo; i++) {
+        double finalTotal = (allCred[i]*singleGrades[i]); // her dersin kredisi * o dersten alınan not değerlerinin toplamı
+        }
+    
+    }
+
+    private static void startDouble() {
+
+    } 
+
 }
